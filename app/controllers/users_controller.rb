69c4d025a3bfdef0858319b547aba07f89d87f47
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-21 02:08:08 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @diaries = user.tweets
+    @diaries = user.diaries
   end
 end
