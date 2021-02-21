6f5fb1c81c32a6688e5a5a5201406fd950cfa057
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-21 22:47:51 +0900

    検索機能

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,4 +3,12 @@ class Diary < ApplicationRecord
   validates :weight, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Diary.where('day LIKE(?)', "%#{search}%")
+    else
+      Diary.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'diaries#index'
   resources :diaries do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
