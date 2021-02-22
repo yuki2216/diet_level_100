b62ae84f71232e0d88fd8d39a744bb688c5301c6
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-22 21:43:43 +0900

    画像投稿機能

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -3,9 +3,11 @@ class Diary < ApplicationRecord
   validates :weight, presence: true
   belongs_to :user
   has_many :comments
+  has_one_attached :image
+
   with_options presence: true do
     validates :day , format: { with: /\A[0-9]+\z/, message: "半角数字で入力してください" }
-    validates :weight, format: { with: /\A\d{1,3}[.]\d{1,2}\z/, message: "第二位までの小数点を含む半角数字で入力してください" }
+    validates :weight, format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/, message: "半角数字で入力してください" }
   end
 
   def self.search(search)
