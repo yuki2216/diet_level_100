cbc3cdb2bd3a08d544bc185978e8491387446903
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-26 19:50:15 +0900

    プレビュー機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("chartkick")
 require("chart.js")
+require('./preview')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
